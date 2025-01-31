0132777ae37bfb4c1ed983099c6889090ed392e9
Drozdov Evgeny <drozdove912@gmail.com>
2025-01-31 15:38:04 +0200

    ComponentCategory

--- src/components/category/Category.tsx
+++ src/components/category/Category.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import styles from "./Category.module.scss";
 import Sort from "../sort/Sort";
+import { category } from "../../data/data";
 const Category = () => {
   return (
     <div className={styles.category}>
-      <div className={styles.item}>Все</div>
-      <div className={styles.item}>Мясные</div>
-      <div className={styles.item}>Вегатарианские</div>
-      <div className={styles.item}>Гриль</div>
-      <div className={styles.item}>Острые</div>
-      <div className={styles.item}>Закрытые</div>
+      {category.map((categ, index) => {
+        return (
+          <div key={index} className={styles.item}>
+            {categ}
+          </div>
+        );
+      })}
       <div className={styles.item}>
         <Sort />
       </div>
--- src/components/sort/Sort.tsx
+++ src/components/sort/Sort.tsx
@@ -1,11 +1,27 @@
 import React, { useState } from "react";
-
+import styles from "./Sort.module.scss";
+import { category } from "../../data/data";
 const Sort = () => {
-  const [sort, setSort] = useState("Все");
+  const [sort, setSort] = useState("");
   return (
-    <div>
-      <label htmlFor="sort"></label>
-      <select name="sort" id="sort" value={sort}></select>
+    <div className={styles.sort}>
+      <label htmlFor="sort">Сортировать по</label>
+      <select
+        name="sort"
+        id="sort"
+        value={sort}
+        onChange={(event) => {
+          setSort(event.target.value);
+        }}
+      >
+        {category.map((categ, index) => {
+          return (
+            <option key={index} value={categ}>
+              {categ}
+            </option>
+          );
+        })}
+      </select>
     </div>
   );
 };
