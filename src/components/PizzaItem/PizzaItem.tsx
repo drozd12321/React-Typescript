5f7664c240a7333f0c80a4d01f05f6526a643c7d
Drozdov Evgeny <drozdove912@gmail.com>
2025-02-04 17:16:46 +0200

    type and price state

--- src/components/PizzaItem/PizzaItem.tsx
+++ src/components/PizzaItem/PizzaItem.tsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { useContext } from "react";
 import styles from "./PizzaItem.module.scss";
 import { PizzaItemProps } from "../interfaces/interface";
 import { MyContext } from "../../data/Context";
 import { TYPE } from "../../data/data";
 const PizzaItem: React.FC<PizzaItemProps> = (props) => {
+  const [count, setCount] = useState<number>(0);
   const context = useContext(MyContext)?.data;
-  console.log(context);
+  const [selectedSize, setSelectedSize] = useState<string | null>(null);
+  const [selectedType, setSelectedType] = useState<number | null>(null);
+
+  function handleSizeSelected(size: string) {
+    setSelectedSize(size);
+  }
+  function handleTypePizza(type: number) {
+    setSelectedType(type);
+  }
+  console.log(selectedSize);
   return (
     <div className={styles.containerPizza}>
       <div className={styles.pizzaImg}>
@@ -16,25 +26,39 @@ const PizzaItem: React.FC<PizzaItemProps> = (props) => {
       <div className={styles.charcter}>
         <div className={styles.item}>
           {props.types.map((typ, ind) => (
-            <div key={ind} className={`${styles.itemChld} `}>
+            <div
+              key={ind}
+              onClick={() => handleTypePizza(typ)}
+              className={`${styles.itemChld} ${
+                selectedType === typ ? styles.active : ""
+              }`}
+            >
               <span>{TYPE[typ]}</span>
             </div>
           ))}
         </div>
         <div className={styles.size}>
           {Object.entries(props.sizes).map(([key, size]) => (
-            <div key={key} className={`${styles.sizeChld}`}>
-              <span>{key}</span>
+            <div
+              key={key}
+              className={`${styles.sizeChld} ${
+                selectedSize === key ? styles.active : ""
+              }`}
+            >
+              <span onClick={() => handleSizeSelected(key)}>{key}</span>
             </div>
           ))}
         </div>
       </div>
       <div className={styles.check}>
         <div className={styles.checkChld}>
-          <span>Цена</span>
+          <span>
+            Цена:{" "}
+            {selectedSize ? props.sizes[selectedSize].replace("₽", "") : 0}
+          </span>
         </div>
-        <div className={`${styles.checkChld} ${styles.active}`}>
-          <span>Добавить</span>
+        <div className={`${styles.checkChld}`}>
+          <span>Добавить {count === 0 ? "" : count}</span>
         </div>
       </div>
     </div>
